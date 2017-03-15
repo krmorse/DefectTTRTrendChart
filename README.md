5a654c56fd651e68cb502cc449b84fd8bc0be38c
Kyle Morse <kmorse@rallydev.com>
2017-03-14 20:11:19 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+Defect TTR Trend Chart
+=========================
+
+## Overview
+
+
+## License
+
+Defect TTR Trend Chart is released under the MIT license.  See the file [LICENSE](./LICENSE) for the full text.
+
+##Documentation for SDK
+
+You can find the documentation on our help [site.](https://help.rallydev.com/apps/2.1/doc/)
---
+++ TTRCalculator.js
@@ -0,0 +1,53 @@
+Ext.define('TTRCalculator', {
+
+    config: {
+        bucketBy: '',
+        priorities: []
+    },
+
+    constructor: function(config) {
+        this.initConfig(config);
+    },
+
+    prepareChartData: function(store) {
+        var data = this._groupData(store),
+        categories = _.keys(data),
+        priorities = this.priorities;
+
+        var series = {};
+        _.each(categories, function(category) {
+            var group = data[category];
+            var recordsByPriority = _.groupBy(group, function(record) {
+                return record.get('Priority') || 'None';
+            }, this);
+            _.each(priorities, function(priority) {
+                series[priority] = series[priority] || [];
+                var records = recordsByPriority[priority] || [];
+                var totalTTR = 0;
+                _.each(records, function(record) {
+                    totalTTR += moment(record.get('ClosedDate')).diff(moment(record.get('CreationDate')), 'days');
+                });
+                series[priority].push(totalTTR / Math.max(records.length, 1));
+            }, this);
+        }, this);
+
+        return {
+            categories: categories,
+            series: _.map(priorities, function(value) {
+                return {
+                    name: value,
+                    type: 'line',
+                    data: series[value]
+                };
+            }, this)
+        };
+    },
+
+    _groupData: function(store) {
+        return _.groupBy(store.getRange(), function(record) {
+            if (this.bucketBy === 'week') {
+                return moment(record.get('ClosedDate')).startOf('isoWeek').format('MMM D');
+            }
+        }, this);
+    }
+});
